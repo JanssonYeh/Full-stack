4c8015dcf5c54fd25dfad50cc96b988f2406d8c3
jansson <jansson@outlook.com>
2024-11-09 19:33:34 +1100

    update lambda and delegate

---
+++ Backend/2. C#/code/Lambda/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action f1 = delegate ()
+            {
+                Console.WriteLine("我是一条无参数无返回值的匿名委托");
+            };
+            f1();
+
+
+            Action<string, int> f2 = delegate (string n, int i)
+            {
+                Console.WriteLine($"n={n},i={i},我是一条有参数无返回值的Action匿名委托");
+            };
+            f2("hello", 10);
+
+            Func<int, int, int> f3 = delegate (int i, int j)
+            {
+                Console.WriteLine("我是一条有参数有返回值的匿名委托");
+                return i + j;
+            };
+            f3(1, 2);
+
+        }
+    }
+}
