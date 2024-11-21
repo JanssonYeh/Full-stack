23bab39cc55fa93ef550cd88d3df2f5ff3af415c
jansson <jansson@outlook.com>
2024-11-21 20:23:37 +1100

    update .net

---
+++ Backend/3. ASP.NET/code/1. 依赖注入/1. 在NET中使用DI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+public interface IMessageWrite
+{
+    void Write(string message);
+}
+
+public class MessageWrite: IMessageWrite
+{
+    public void Write(string message)
+    {
+        Console.WriteLine($"The message is {message}");
+    }
+}
+
+class program
+{
+    static void Main()
+    {
+        var service = new ServiceCollection();
+        service.AddTransient<IMessageWrite, MessageWrite>();
+
+        using (var serviceProvider = service.BuildServiceProvider())
+        {
+            var user = serviceProvider.GetService<IMessageWrite>();
+            user.Write("124153");
+        }
+    }
+}
---
+++ Backend/3. ASP.NET/code/1. 依赖注入/2. 服务的生命周期/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+public interface IMessageWrite
+{
+    void Write(string message);
+}
+
+public class MessageWrite:IMessageWrite
+{
+    public void Write(string message)
+    {
+        Console.WriteLine($"this is {message}");
+    }
+}
+
+class program
+{
+    static void Main()
+    {
+        var services = new ServiceCollection();
+        services.AddTransient<IMessageWrite, MessageWrite>();
+
+        var serviceProvide = services.BuildServiceProvider();
+
+       
+    }
+}
